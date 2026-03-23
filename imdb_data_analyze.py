195bba6e7c75770e10c15ab930d225acc0f67f21
İbrahim Emre Yıldız <iemreis803@gmail.com>
2026-03-23 14:50:37 +0300

    Alıştırma

--- imdb_data_analyze.py
+++ imdb_data_analyze.py
@@ -19,5 +19,6 @@ result= df[(df["Year"]>=2014) & (df["Year"]<= 2015)]["Title"]
 df["imdbVotes"] = df["imdbVotes"].str.replace(",", "").pipe(pd.to_numeric, errors="coerce") # pipe string işleminin çıktısını to numeric'e gönderiyor.
 df["imdbRating"] = pd.to_numeric(df["imdbRating"], errors="coerce")
 result= df[(df["imdbVotes"]>10000) & (df["imdbRating"]>=1.8) & (df["imdbRating"]<=2.9)]
+
 #result= df.head()
 print(result)
